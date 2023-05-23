826a529bc6461f0831cbaf5afd28c72f179dff20
Dar-ya218 <dasharoura13465@gmail.com>
2023-05-23 10:14:56 +0200

    ejercicio_2_y_3

--- build/index.js
+++ build/index.js
@@ -8,18 +8,68 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+const jokeValor = [];
+let jokeObject = {
+    score: 0,
+};
 function fetchJoke() {
     return __awaiter(this, void 0, void 0, function* () {
         const response = yield fetch('https://icanhazdadjoke.com/', {
             headers: { "Accept": "application/json" }
         });
-        const data = yield response.json();
+        const data = yield response.json(); // que formatea y guarde la respuesta en formato json
         return data.joke;
     });
 }
 ;
 const siguienteJoke = document.getElementById("siguiente_joke");
 siguienteJoke === null || siguienteJoke === void 0 ? void 0 : siguienteJoke.addEventListener("click", () => __awaiter(void 0, void 0, void 0, function* () {
+    if (jokeObject.score !== 0) {
+        jokeValor.push(jokeObject);
+        console.log('jokeValor', jokeValor);
+    }
     const joke = yield fetchJoke();
     console.log(joke);
+    const jokeElement = document.getElementById("result");
+    if (jokeElement instanceof HTMLElement) {
+        jokeElement.innerHTML = joke; // Asignar el chiste al elemento con id "jokeElement"
+    }
+    const btnValor = document.getElementById("btn");
+    btnValor.style.display = "block";
+    jokeObject = {
+        joke: joke,
+        score: 0,
+        fecha: date()
+    };
+    console.log('jokeObject', jokeObject);
+    const btn1 = document.getElementById("uno");
+    const btn2 = document.getElementById("dos");
+    const btn3 = document.getElementById("tres");
+    btn1 === null || btn1 === void 0 ? void 0 : btn1.addEventListener("click", () => {
+        jokeObject.score = 1;
+        console.log('jokeObject', jokeObject);
+    });
+    btn2 === null || btn2 === void 0 ? void 0 : btn2.addEventListener("click", () => {
+        jokeObject.score = 2;
+        console.log('jokeObject', jokeObject);
+    });
+    btn3 === null || btn3 === void 0 ? void 0 : btn3.addEventListener("click", () => {
+        jokeObject.score = 3;
+        console.log('jokeObject', jokeObject);
+    });
 }));
+function date() {
+    const d = new Date();
+    let day = d.toISOString();
+    return day;
+}
+;
+/* const dos: any = document.getElementById("dos")!;
+dos.addEventListener("click", () => {
+    reportAcudits.push({
+      joke: document.getElementById("chiste")?.innerHTML,
+      score: 2,
+      date: dateToday,
+    });
+    console.log(reportAcudits);
+  }); */ 
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,76 @@
-async function fetchJoke() :Promise<string> {
-    const response = await  fetch('https://icanhazdadjoke.com/', {
+const jokeValor: Array<object> = [];
+let jokeObject: any = {
+    score: 0,
+}
+
+async function fetchJoke() :Promise<string> { // esta en pausa, hasta que cumpla la promesa
+    const response = await  fetch('https://icanhazdadjoke.com/',  { //espera hasta que traiga la respuesta
         headers: {"Accept": "application/json"}
     }  );
-    const data = await response.json();
+    const data = await response.json(); // que formatea y guarde la respuesta en formato json
     return data.joke 
 };
 const siguienteJoke = document.getElementById("siguiente_joke");
 siguienteJoke?.addEventListener("click", async()=>{
+    if (jokeObject.score !== 0) {
+       jokeValor.push(jokeObject);
+       console.log('jokeValor', jokeValor) 
+    }
     const joke = await fetchJoke();
     console.log(joke)
+
+    const jokeElement = document.getElementById("result");
+  if (jokeElement instanceof HTMLElement) {
+    jokeElement.innerHTML = joke; // Asignar el chiste al elemento con id "jokeElement"
+  }
+  const btnValor = document.getElementById("btn") as HTMLDivElement;
+  btnValor.style.display = "block";
+
+   jokeObject = {
+    joke: joke,
+    score: 0,
+    fecha: date()
+  }
+  console.log('jokeObject',jokeObject);
+  
+  const btn1: any = document.getElementById("uno");
+  const btn2: any = document.getElementById("dos");
+  const btn3: any = document.getElementById("tres");
+  
+  btn1?.addEventListener("click", ()=>{
+      jokeObject.score = 1;
+      console.log('jokeObject', jokeObject)
+  });
+  btn2?.addEventListener("click", ()=>{
+      jokeObject.score = 2;
+      console.log('jokeObject',jokeObject)
+  });
+  btn3?.addEventListener("click", ()=>{
+      jokeObject.score = 3;
+      console.log('jokeObject',jokeObject)
+  });
 });
+
+function date():string  {
+    const d = new Date();
+    let day = d.toISOString();
+    return day
+};
+
+
+
+
+
+
+
+
+
+/* const dos: any = document.getElementById("dos")!;
+dos.addEventListener("click", () => {
+    reportAcudits.push({
+      joke: document.getElementById("chiste")?.innerHTML,
+      score: 2,
+      date: dateToday,
+    });
+    console.log(reportAcudits);
+  }); */
